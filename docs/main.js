a12dd56d18e9f73296ad5385156f5271bb444855
Lipiu <rosuliviu10@gmail.com>
2025-08-23 03:12:05 +0300

    o iau razna

--- docs/main.js
+++ docs/main.js
@@ -12,3 +12,13 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+//icon loop
+document.addEventListener("DOMContentLoaded", () => {
+  const wrapper = document.querySelector(".scrolling-wrapper");
+  const content = document.querySelector(".scrolling-content");
+
+  const clone = content.cloneNode(true);
+  clone.style.marginLeft = "1.5rem";
+  wrapper.appendChild(clone);
+});
